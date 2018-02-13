b7a3d2fea67d54fb57648b931bed93dceb60601a
ShashidharPK <33952012+ShashidharPK@users.noreply.github.com>
2018-02-13 17:44:30 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -57,9 +57,8 @@ if(j==k)
 marks=marks+2;
 else
 marks=marks-1;
-System.out.println("ANS IS " + k);
-System.out.println("J IS " + j);
-System.out.println(marks);
+
+
 rs.next();
 }catch(Exception e) { System.out.println(e); } 
 
