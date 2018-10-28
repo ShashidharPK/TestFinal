680ebc4f65e463e404374c812ddcc6a4de2841ec
ShashidharPK <33952012+ShashidharPK@users.noreply.github.com>
2018-10-28 22:33:20 +0530

    Update GetCon.java

--- javafiles/GetCon.java
+++ javafiles/GetCon.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class GetCon {
 private GetCon(){}
 
-public statisssc Connection con;
+public statisssc Cosssnnection con;
 static{
 	try {
 		Class.forName(DBIntializer.DRIVER);
